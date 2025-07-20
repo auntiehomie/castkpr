7126d1345fcb217fa723e9cd3c5500aa5dcc80c4
Amanda <auntiehomie@proton.me>
2025-07-20 14:44:36 -0400

    updated next.config.ts to solve pfp issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Allows all HTTPS domains
+      },
+    ],
+    // Or if you prefer to be more specific with domains:
+    domains: [
+      'res.cloudinary.com',
+      'imagedelivery.net', 
+      'cdn.stamp.fyi',
+      'i.imgur.com',
+      'api.dicebear.com',
+      'storage.googleapis.com',
+      'lh3.googleusercontent.com',
+      // Add other common Farcaster PFP domains
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
